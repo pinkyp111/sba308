8d428507f614d0b81708f00927b0156420cd88d7
Pinky Pappachan <pinkypappachan111@gmail.com>
2024-05-19 14:03:21 -0700

    added changes to home page

--- js/home.js
+++ js/home.js
@@ -1,8 +1,91 @@
 function generateHomePage() {
     return `
-        <div class="home-page">
-            <p>Home Page</p>
-        </div>
+    <div class="home-page">
+    <p align="none"><br><span style="color: crimson; font-weight: bold;">Announcement:</span>
+        <br>The Peoria Main Library and Sunrise Mountain Library will be closed Monday, May. 27, in observance of
+        Memorial Day.
+        The buildings will reopen on the following Tuesday, May 28, at 9 a.m. Library materials that would have been due
+        on May 27 will not be due until the next business day.The outside bookdrops will be unavailable during the
+        closure.Enriching Peoria’s evolving community through equal access to information, while offering inclusive
+        programs,
+        innovative technology and diverse collections.
+    </p>
+    <div class="ppl3-parts-1cp">
+        <h4 style="color: crimson;">Holiday & Early Closures</h4>
+        <table>
+
+            <tbody>
+                <tr>
+                    <td>Monday, Dec. 25, 2023</td>
+                    <td>Winter Holiday</td>
+                </tr>
+                <tr>
+                    <td>Monday, Jan. 1, 2024</td>
+                    <td>New Year's Day</td>
+                </tr>
+                <tr>
+                    <td>Monday, Jan. 15, 2024</td>
+                    <td>Martin Luther King Jr. Day</td>
+                </tr>
+                <tr>
+                    <td>Monday, Feb. 19, 2024</td>
+                    <td>President's Day</td>
+                </tr>
+                <tr>
+                    <td>Monday, May 27, 2024</td>
+                    <td>Memorial Day</td>
+                </tr>
+                <tr>
+                    <td>Wednesday, Jun. 19, 2024</td>
+                    <td>Juneteenth Holiday</td>
+                </tr>
+                <tr>
+                    <td>Thursday, Jul. 4, 2024</td>
+                    <td>Independence Day</td>
+                </tr>
+                <tr>
+                    <td>Monday, Sept. 2, 2024</td>
+                    <td>Labor Day</td>
+                </tr>
+                <tr>
+                    <td>Monday, October 14, 2024</td>
+                    <td>Library Staff Closure</td>
+                </tr>
+                <tr>
+                    <td>Thursday, Oct. 31, 2024</td>
+                    <td>Close at 5 p.m.</td>
+                </tr>
+                <tr>
+                    <td>Monday, Nov. 11, 2024</td>
+                    <td>Veterans Day</td>
+                </tr>
+                <tr>
+                    <td>Wednesday, Nov. 27, 2024</td>
+                    <td>Close at 5 p.m. (City Holiday)</td>
+                </tr>
+                <tr>
+                    <td>Thursday, Nov. 28, 2024</td>
+                    <td>Thanksgiving</td>
+                </tr>
+                <tr>
+                    <td>Friday, Nov. 29, 2024</td>
+                    <td>Thanksgiving</td>
+                </tr>
+                <tr>
+                    <td>Tuesday, Dec. 24, 2024</td>
+                    <td>Close at 12 p.m.</td>
+                </tr>
+                <tr>
+                    <td>Wednesday, Dec. 25, 2024</td>
+                    <td>Winter Holiday</td>
+                </tr>
+                <tr>
+                    <td>Wednesday, Jan. 1, 2025</td>
+                    <td>New Year's Day</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 `;
 }
 
