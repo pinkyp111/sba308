6a911764d40348e8fd20096615d08928fd9c18bd
Pinky Pappachan <pinkypappachan111@gmail.com>
2024-05-21 12:27:53 -0700

    Latest changes

--- js/community.js
+++ js/community.js
@@ -8,7 +8,7 @@ function generateCommunityPage() {
      to invest in communities across the state.The foundation focuses on various areas including arts & culture, community improvement, environment & sustainability, 
      health innovations, sports & athletics, and quality education. It also has initiatives for Asian American, Black, Latino,
      LGBTQ, and Native American philanthropy.ACF has regional offices across Arizona, allowing them to serve specific communities and address local needs effectively.
-    They offer a range of services for donors, nonprofits, and communities, aiming to create a lasting impact</p>
+    They offer a range of services for donors, nonprofits, and communities, aiming to create a lasting impact.</p>
     
     <iframe src="https://giphy.com/embed/fXnx6vSSrzY92rTONJ" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/IntoAction-fXnx6vSSrzY92rTONJ">via GIPHY</a></p>
     
